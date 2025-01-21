72a377362dc9851c04a38afb71bbfafd989406a2
ryana <ryanadolph7@gmail.com>
2025-01-21 10:55:29 -0700

    another commit for the 1 second interrupt changed the delay loop values in R15

--- app/main.asm
+++ app/main.asm
@@ -79,6 +79,7 @@ RESET       mov.w   #__STACK_END,SP         ; Initialize stack pointer
 StopWDT     mov.w   #WDTPW+WDTHOLD,&WDTCTL  ; Stop WDT
 SetupP1     bic.b   #BIT0,&P1OUT            ; Clear P1.0 output
             bis.b   #BIT0,&P1DIR            ; P1.0 output
+            xor.b   #BIT0, &P1OUT
 
 setup_P6    bic.b   #BIT6, &P6OUT           ; clear P6.6
             bis.b   #BIT6, &P6DIR           ; P6.6 as output
@@ -100,7 +101,7 @@ setup_timer_B0
 
 Mainloop:    xor.b   #BIT0,&P1OUT            ; Toggle P1.0 every 0.1s
 
-Wait:        mov.w   #50000,R15              ; Delay to R15, inner loop
+Wait:        mov.w   #24000, R15              ; Delay to R15, inner loop
             mov.w   #7, R14                 ; Outer loop delay
 
 ; almost correct, need to check on oscilliscope to verify delay == 1 sec
@@ -108,7 +109,7 @@ call_L1:     dec.w   R14
             cmp.w   #000000000h, R14        ; check if R14 == 0
             jz     Mainloop                 ; if yes, then jump back to main
             
-;L1:          dec.w   R15                     ; Decrement R15
+L1:          dec.w   R15                     ; Decrement R15
             jnz     L1                      ; Delay over?
             jmp     call_L1                 ; Again
             NOP
